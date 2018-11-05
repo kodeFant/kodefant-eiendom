d4e81bbb3b3b2980f38a0b4dfc693431541d8a61
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-11-05 23:59:15 +0100

    .htaccess plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'Gatsby Modern Starter',
     language: 'en',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-htaccess-redirects`,
+      options: {
+        prefix: "<IfModule mod_rewrite.c>\nRewriteEngine On",
+        suffix: "</IfModule>",
+      },
+    },
+
+  ],
 };
