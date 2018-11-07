97c8bf65a9277e483c852bac3f9548bcb0b20361
kodeFant <lars.lillo@gmail.com>
2018-11-07 15:50:54 +0100

    Build folder included

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,12 +2,11 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/kodefant/kode/eiendom/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/kodefant/kode/eiendom/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/kodefant/kode/eiendom/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-om-js": () => import("/Users/kodefant/kode/eiendom/src/pages/om.js" /* webpackChunkName: "component---src-pages-om-js" */),
   "component---src-pages-eiendommer-js": () => import("/Users/kodefant/kode/eiendom/src/pages/eiendommer.js" /* webpackChunkName: "component---src-pages-eiendommer-js" */),
-  "component---src-pages-kontakt-js": () => import("/Users/kodefant/kode/eiendom/src/pages/kontakt.js" /* webpackChunkName: "component---src-pages-kontakt-js" */)
+  "component---src-pages-index-js": () => import("/Users/kodefant/kode/eiendom/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-kontakt-js": () => import("/Users/kodefant/kode/eiendom/src/pages/kontakt.js" /* webpackChunkName: "component---src-pages-kontakt-js" */),
+  "component---src-pages-om-js": () => import("/Users/kodefant/kode/eiendom/src/pages/om.js" /* webpackChunkName: "component---src-pages-om-js" */)
 }
 
 exports.data = () => import("/Users/kodefant/kode/eiendom/.cache/data.json")
