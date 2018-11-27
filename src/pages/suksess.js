7bec149304612d639516ed6d29d8da9c395c485a
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-11-27 18:21:55 +0100

    Success file added

---
+++ src/pages/suksess.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './kontakt.module.scss'
+
+import Layout from '../components/layout/layout'
+
+const ContactPage = () => (
+  <Layout>
+    <div className={styles.container}>
+      Takk for henvendelsen. Jeg svarer deg så fort jeg har mulighet.
+    </div>
+  </Layout>
+)
+
+export default ContactPage
