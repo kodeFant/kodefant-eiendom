4b01013e7cebf75aee45fc2bd3563a2cd0788283
Lars Lillo Ulvestad <lars.lillo@gmail.com>
2018-11-27 16:47:04 +0100

    Netlify form fix

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Formik, Field, Form } from 'formik'
-import mg from './mg'
 
 import styles from './contactForm.module.scss'
 
@@ -18,28 +17,8 @@ const contactForm = () => (
         subject: 'You temp!',
         message: "I'm also temp",
       }}
-      /*  onSubmit={({ subject, message }) => {
-        mg.messages
-          .create('mg.kodefant.no', {
-            from: `DinEiendom-henvendelse <postmaster@mg.kodefant.no>`,
-            to: ['lillo@kodefant.no'],
-            subject: subject,
-            text: message,
-          })
-          .then((
-            msg // eslint-disable-next-line no-console
-          ) => console.log('msg', msg))
-          .catch((
-            err // eslint-disable-next-line no-console
-          ) => console.log('err', err))
-      }} */
       render={() => (
-        <Form
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
+        <Form name="contact" netlify="true">
           <div className={styles.contactGrid}>
             <Field type="text" name="name" placeholder="Ditt navn" />
             <Field type="email" name="email" placeholder="Din epost" />
